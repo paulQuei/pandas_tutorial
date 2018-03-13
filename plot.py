00fc33210b239f724fc70cc6590a9870f70966f1
paulquei <paulquei@gmail.com>
2018-03-13 17:55:45 +0800

    Change plot example

--- plot.py
+++ plot.py
@@ -1,10 +1,8 @@
 # plot.py
 
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-df = pd.DataFrame(np.random.randint(0, 100, 4*12).reshape(-1, 4),
-                  columns=['A', 'B', 'C', 'D']);
-df.plot.bar()
+data = pd.read_csv("data/housing.csv")
+data.hist(bins=50, figsize=(15, 12))
 plt.show()
